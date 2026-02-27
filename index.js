c5816e7aaab8c14700698c429369effb6f5f6b11
Ilia Maksimov <maksimov.spb@gmail.com>
2026-02-27 14:34:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  console.log("WebMAX is coming soon! Stay tuned.");
+const WebMAXEngine = require("./engine");
+
+const engine = new WebMAXEngine("web");
+
+module.exports = {
+  View: (props, ...children) =>
+    engine.createElement("View", props, ...children),
+  Text: (props, ...children) =>
+    engine.createElement("Text", props, ...children),
+  Action: (props, ...children) =>
+    engine.createElement("Action", props, ...children),
+
+  compile: (platform) => new WebMAXEngine(platform),
 };
